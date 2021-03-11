28595aad278641f0c33debd8eef679b11b95ebdb
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-03-11 00:03:32 -0600

    saltos de línea!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
@@ -20,25 +22,25 @@ public class Main {
             switch(opcion){
                 case 1:
                     String[] premios = new String[5];
-                    premios[0] = "que en exactamente 10 años, tu pack se filtrará por accidente y se volverá viral," +
-                            "lo que te traerá muchas oportunidades como poder tener tu propio reality show," +
-                            "lo cual rechazarás porque siempre dign@, nunca indign@.";
-                    premios[1] = "que el 30 de Junio tocarán a tu puerta para fumigar, así que te irás " +
-                            "a la tienda de la esquina por una cocacola. " +
+                    premios[0] =  "que en exactamente 10 años, tu pack se filtrará por accidente y se volverá viral,\n" +
+                            "lo que te traerá muchas oportunidades como poder tener tu propio reality show,\n" +
+                            "lo cual rechazarás porque siempre dign@, nunca indign@.\n";
+                    premios[1] = "que el 30 de Junio tocarán a tu puerta para fumigar, así que te irás\n " +
+                            "en chanclas a la tienda de la esquina por una cocacola de vidrio.\n " +
                             "De repente entrará tu crush a la tienda, ignorándote " +
-                            "por completo y le preguntará al tendero si hay Nutella.";
-                    premios[2]= "en el 2,040 tú y tu familia huirán de Guatemala debido a la debacle político y económico que ha provocado la dictadura" +
-                            "de Neto Bran y para cubrir los gastos venderás tu cuerpo a una corporación para que realice experimentos." +
-                            "Te transformarán en un cyborg, lo que extenderá tu tiempo de vida por 500 años más, de los cuales utilizarás los primeros" +
-                            "20 prestando servicio militar para ayudar a establecer un nuevo orden mundial.";
-                    premios[3]= "esta noche no dormirás porque necesitas estudiar para tu próximo parcial, pero pasarás la mitad del tiempo " +
-                            "viendo videos de bromas a gatos y stalkeando el instagram de tu crush. ";
-                    premios[4]= "ganaste un ¡CÓMODÍN! Felicidades, has ganado la oportunidad de elegir un fragmento de tu futuro. " +
-                            "Todo lo que imagines en los próximos 10 segundos se hará realidad. A disfrutar de la" +
-                            "vida se ha dicho.";
+                            "por completo y\n le preguntará al tendero si hay Nutella. Jajajaja\n\n";
+                    premios[2]= "en el 2,040 tú y tu familia huirán de Guatemala debido a la debacle político y económico\n que ha provocado la dictadura" +
+                            "de Neto Bran y para cubrir los gastos venderás tu cuerpo a una corporación\n para que realice experimentos.\n" +
+                            "Te transformarán en un cyborg, lo que extenderá tu tiempo de vida por 500 años más,\n de los cuales utilizarás los primeros" +
+                            "20 prestando servicio militar para ayudar a\nestablecer un nuevo orden mundial.\n\n";
+                    premios[3]= "esta noche no dormirás porque necesitas estudiar para tu próximo parcial, pero pasarás la mitad\n del tiempo " +
+                            "viendo videos de bromas a gatos y stalkeando a tu crush en instagram.\n\n ";
+                    premios[4]= "ganaste un ¡CÓMODÍN! Felicidades, has ganado la oportunidad de elegir un fragmento de tu futuro.1\n " +
+                            "Todo lo que imagines en los próximos 10 segundos se hará realidad.\n A disfrutar de la" +
+                            "vida se ha dicho\n\n";
                     int random = (int) (Math.random()*5);
 
-                    System.out.println("tu visión para un futuro próximo o lejano es que " + premios[random] + ", jajajajajaja");
+                    System.out.println("tu visión para un futuro próximo o lejano es que " + premios[random]);
                     break;
                 case 2:
                     System.out.println("Hasta pronto...");
